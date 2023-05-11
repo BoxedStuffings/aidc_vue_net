48b352944ca4298c984d95f50ddd4df45f577fc1
OwO11 <celsius_chia_zhan_yao@connect.ite.edu.sg>
2023-05-11 18:33:05 +0800

    Added router

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import TV from '../components/TV.vue'
+import MainSelection from '../components/MainSelection.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+
+  routes: [
+    {
+      path: '/',
+      name: 'Tv',
+      component: TV
+    },
+    {
+      path: '/MainSelection',
+      name: 'Main Selection',
+      component: MainSelection
+    }
+  ]
+})
+
+export default router
