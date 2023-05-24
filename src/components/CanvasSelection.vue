94ef6d81bfea557bb1ff6a0136ee6c1898a639c1
OwO11 <celsius_chia_zhan_yao@connect.ite.edu.sg>
2023-05-24 11:28:40 +0800

    Testing telegram user data

--- src/components/CanvasSelection.vue
+++ src/components/CanvasSelection.vue
@@ -106,9 +106,10 @@ export default {
     <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '15%'}">CanvasHeight: {{ canvasHeight }}</h4>
     <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '20%'}">ToolBarHeight: {{ wrapperHeight/90*100 }}</h4>
     <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '25%'}">CanvasLarger?: {{ big }}</h4> -->
-    <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '10%'}">UserObject: {{ store.telegramWebAppInfo }}</h4>
-    <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '15%'}">QueryID: {{ store.telegramWebAppInfo.query_id }}</h4>
+    <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '10%'}">QueryID: {{ store.telegramWebAppInfo.query_id }}</h4>
     <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '20%'}">UserID: {{ store.telegramWebAppInfo.user.id }}</h4>
+    <h4 :style="{'z-index': 2, 'position': 'absolute', 'top': '30%'}">UserObject: {{ store.telegramWebAppInfo }}</h4>
+
     <!-- TBR -->
 
     <toolbar class="toolbar"></toolbar>
