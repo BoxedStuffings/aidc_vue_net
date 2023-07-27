9ca0791125143c9a58dbc386da4f2cb6e333e1d6
BoxedStuffings <boxedstuffings@gmail.com>
2023-07-27 14:52:56 +0800

    canvas changes

--- src/components/CanvasSelection.vue
+++ src/components/CanvasSelection.vue
@@ -480,14 +480,15 @@ export default {
   beforeMount() {
     const mainButton = () => {
       store.setMediaType('Canvas')
-      this.canvas.toBlob((blob) => {
-        let file = new File([blob], 'AIDC_Canvas')
-        store.uploadCanvas(file)
-        
-        Telegram.WebApp.offEvent('backButtonClicked', backButton)
-        this.telegramMainButton.hide()
-        this.$router.push('/ScheduleSelection')
-      })
+      // this.canvas.toBlob((blob) => {
+      //   let file = new File([blob], 'AIDC_Canvas')
+      //   store.uploadCanvas(file)
+
+
+      // })
+      Telegram.WebApp.offEvent('backButtonClicked', backButton)
+      this.telegramMainButton.hide()
+      this.$router.push('/ScheduleSelection')
     }
 
     const backButton = () => {
