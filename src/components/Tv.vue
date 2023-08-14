bebb0966de4e1cf410e4bc7dc02ba64c537f8537
Yongqing Wang <s10222015@connect.np.edu.sg>
2023-08-14 15:49:29 +0800

    test

--- src/components/Tv.vue
+++ src/components/Tv.vue
@@ -14,7 +14,7 @@ export default {
   data() {
     return {
       store,
-      checkTheme: '../src/assets/icons/light/check.svg',
+      checkTheme: '../assets/check.svg',
       telegramMainButton: Telegram.WebApp.MainButton,
       telegramBackButton: Telegram.WebApp.BackButton,
       loading: 0
@@ -178,7 +178,7 @@ export default {
         <!-- Image outline for selection -->
         <div class="tv-card-imageOutline" :class="{selected : store.findIndexOfSelectedTv(TV) >= 0 }">
           <!-- Card check mark -->
-          <img class="tv-card-imageCheck" :class="{selected : store.findIndexOfSelectedTv(TV) >= 0 }" :src="'../src/assets/icons/light/check.svg'">
+          <img class="tv-card-imageCheck" :class="{selected : store.findIndexOfSelectedTv(TV) >= 0 }" src="../assets/check.svg">
           <!-- Display image -->
           <img :class="{selected : store.findIndexOfSelectedTv(TV) >= 0 }" src="../assets/tv.png">
         </div>
